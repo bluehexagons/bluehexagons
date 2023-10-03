072dc7c1765a296119e523a4ee77898a15669a85
bluehexagons <bluehexagons@users.noreply.github.com>
2023-10-03 15:57:02 -0500

    Clean some URLs, fix some CSS/HTML, target ES6, start using npm

--- html/assets/js.js
+++ html/assets/js.js
@@ -1,57 +1,57 @@
 'use strict';
-var intro = document.getElementById('intro');
+const intro = document.getElementById('intro');
 if (intro) {
-    var testPause_1 = function () {
+    const testPause = () => {
         if (intro.currentTime > 12.33) {
             intro.pause();
         }
     };
-    intro.addEventListener('ended', function () {
-        intro.addEventListener('timeupdate', testPause_1);
+    intro.addEventListener('ended', () => {
+        intro.addEventListener('timeupdate', testPause);
         intro.currentTime = 0;
         intro.play();
     });
-    var togglePlay = function () {
+    const togglePlay = () => {
         if (intro.paused) {
             intro.muted = false;
-            intro.removeEventListener('timeupdate', testPause_1);
+            intro.removeEventListener('timeupdate', testPause);
             intro.play();
         }
         else {
             intro.pause();
         }
     };
-    intro.addEventListener('timeupdate', testPause_1);
+    intro.addEventListener('timeupdate', testPause);
     intro.addEventListener('click', togglePlay);
 }
-var reels = document.getElementsByClassName('reel');
-var _loop_1 = function (i) {
-    var reelContainerContainer = reels[i];
-    var reelContainer = document.createElement('div');
-    var reel = document.createElement('video');
-    var videoDir = 'assets/clips/';
-    var videos = ['carbon1.mp4', 'xenon1.mp4', 'silicon1.mp4', 'helium1.mp4', 'xenon2.mp4'];
-    var videoIndex = 1;
-    var swapping = false;
+const reels = document.getElementsByClassName('reel');
+for (let i = 0; i < reels.length; i++) {
+    const reelContainerContainer = reels[i];
+    const reelContainer = document.createElement('div');
+    const reel = document.createElement('video');
+    const videoDir = 'assets/clips/';
+    const videos = ['carbon1.mp4', 'xenon1.mp4', 'silicon1.mp4', 'helium1.mp4', 'xenon2.mp4'];
+    let videoIndex = 1;
+    let swapping = false;
     reel.width = 480;
     reel.height = 270;
     reel.muted = true;
     reel.src = videoDir + videos[0];
     reel.play();
-    reel.addEventListener('ended', function () {
+    reel.addEventListener('ended', () => {
         if (swapping) {
             return;
         }
         swapping = true;
         reel.style.opacity = '0';
     });
-    reel.addEventListener('timeupdate', function () {
+    reel.addEventListener('timeupdate', () => {
         if (!swapping && reel.currentTime > reel.duration - 0.2) {
             swapping = true;
             reel.style.opacity = '0';
         }
     });
-    reel.addEventListener('transitionend', function () {
+    reel.addEventListener('transitionend', () => {
         if (!swapping) {
             return;
         }
@@ -60,26 +60,23 @@ var _loop_1 = function (i) {
         reel.load();
         videoIndex = (videoIndex + 1) % videos.length;
     });
-    reel.addEventListener('canplaythrough', function () {
+    reel.addEventListener('canplaythrough', () => {
         if (!swapping) {
             return;
         }
         reel.play();
         swapping = false;
     });
-    reel.addEventListener('play', function () {
+    reel.addEventListener('play', () => {
         reel.style.opacity = '1';
     });
-    reel.addEventListener('mouseenter', function () {
+    reel.addEventListener('mouseenter', () => {
         reel.controls = true;
     });
-    reel.addEventListener('mouseleave', function () {
+    reel.addEventListener('mouseleave', () => {
         reel.controls = false;
     });
     reelContainer.appendChild(reel);
     reelContainerContainer.appendChild(reelContainer);
-};
-for (var i = 0; i < reels.length; i++) {
-    _loop_1(i);
 }
 //# sourceMappingURL=js.js.map
