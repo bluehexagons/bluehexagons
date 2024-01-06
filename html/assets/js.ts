90d575ba2156d63ccbb333e652ffd4559eb7645c
bluehexagons <bluehexagons@users.noreply.github.com>
2024-01-06 15:35:21 -0600

    Contain more

--- html/assets/js.ts
+++ html/assets/js.ts
@@ -1,68 +1,70 @@
 'use strict'
 
-// The video reel is a quick slideshow of a few videos
-// TODO: rewrite using custom elements
-const reels = document.getElementsByClassName('reel') as HTMLCollectionOf<HTMLDivElement>
-for (const reelContainerContainer of reels) {
-  const reelContainer = document.createElement('div')
-  const reel = document.createElement('video')
-  const videoDir = 'assets/clips/'
-  const videos = ['carbon1.mp4', 'xenon1.mp4', 'silicon1.mp4', 'helium1.mp4', 'xenon2.mp4']
-  let videoIndex = 1
-  let swapping = false
-  reel.width = 480
-  reel.height = 270
-  reel.muted = true
-  reel.src = videoDir + videos[0]
-  reel.play()
-  reel.addEventListener('ended', () => {
-    if (swapping) {
-      return
-    }
-    swapping = true
-    reel.style.opacity = '0'
-  })
-  reel.addEventListener('timeupdate', () => {
-    if (!swapping && reel.currentTime > reel.duration - 0.2) {
+const initializeReels = () => {
+  // The video reel is a quick slideshow of a few videos
+  // TODO: rewrite using custom elements
+  const reels = document.getElementsByClassName('reel') as HTMLCollectionOf<HTMLDivElement>
+  for (const reelContainerContainer of reels) {
+    const reelContainer = document.createElement('div')
+    const reel = document.createElement('video')
+    const videoDir = 'assets/clips/'
+    const videos = ['carbon1.mp4', 'xenon1.mp4', 'silicon1.mp4', 'helium1.mp4', 'xenon2.mp4']
+    let videoIndex = 1
+    let swapping = false
+    reel.width = 480
+    reel.height = 270
+    reel.muted = true
+    reel.src = videoDir + videos[0]
+    reel.play()
+    reel.addEventListener('ended', () => {
+      if (swapping) {
+        return
+      }
       swapping = true
       reel.style.opacity = '0'
-    }
-  })
-  reel.addEventListener('transitionend', () => {
-    if (!swapping) {
-      return
-    }
-    reel.currentTime = 0
-    reel.src = videoDir + videos[videoIndex]
-    reel.load()
-    videoIndex = (videoIndex + 1) % videos.length
-  })
-  reel.addEventListener('canplaythrough', () => {
-    if (!swapping) {
-      return
-    }
-    reel.play()
-    swapping = false
-  })
-  reel.addEventListener('play', () => {
-    reel.style.opacity = '1'
-  })
-  reel.addEventListener('mouseenter', () => {
-    reel.controls = true
-  })
-  reel.addEventListener('mouseleave', () => {
-    reel.controls = false
-  })
+    })
+    reel.addEventListener('timeupdate', () => {
+      if (!swapping && reel.currentTime > reel.duration - 0.2) {
+        swapping = true
+        reel.style.opacity = '0'
+      }
+    })
+    reel.addEventListener('transitionend', () => {
+      if (!swapping) {
+        return
+      }
+      reel.currentTime = 0
+      reel.src = videoDir + videos[videoIndex]
+      reel.load()
+      videoIndex = (videoIndex + 1) % videos.length
+    })
+    reel.addEventListener('canplaythrough', () => {
+      if (!swapping) {
+        return
+      }
+      reel.play()
+      swapping = false
+    })
+    reel.addEventListener('play', () => {
+      reel.style.opacity = '1'
+    })
+    reel.addEventListener('mouseenter', () => {
+      reel.controls = true
+    })
+    reel.addEventListener('mouseleave', () => {
+      reel.controls = false
+    })
 
-  reelContainer.appendChild(reel)
-  reelContainerContainer.appendChild(reelContainer)
+    reelContainer.appendChild(reel)
+    reelContainerContainer.appendChild(reelContainer)
+  }
 }
 
 const findLastDot = /\.(?=[^.]*$)/
 const findSuffix = / \(Phone\)\./
 const findThumbs = /(thumbs\/|( |\%20)\(Phone\))/g
-const imageScrollers = document.getElementsByClassName('imageScroller') as HTMLCollectionOf<HTMLDivElement>
-for (const scroller of imageScrollers) {
+const makeImageScroller = (scroller: HTMLDivElement) => {
+  let selectedImageIndex = 0;
   for (const img of scroller.querySelectorAll<HTMLImageElement>(':scope > img')) {
     const link = document.createElement('a')
     const thumbnail = img.src
@@ -72,3 +74,14 @@ for (const scroller of imageScrollers) {
     link.appendChild(img)
   }
 }
+
+const initializeImageScrollers = () => {
+  const imageScrollers = document.getElementsByClassName('imageScroller') as HTMLCollectionOf<HTMLDivElement>
+  for (const div of imageScrollers) {
+    makeImageScroller(div)
+  }
+}
+
+
+initializeReels()
+initializeImageScrollers()
